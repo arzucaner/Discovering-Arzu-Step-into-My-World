10133aeb6c3de052f0fc482e5c3c3421f1cc454f
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-01 23:05:42 +0100

    Update Discovering-me

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("my-work-btn").addEventListener("click", function() {
+    window.location.href = "https://github.com/arzucaner";
+  });
+  
+  var linkedinLink = document.getElementById("linkedin-link");
+  linkedinLink.addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/arzucaner/";
+  });
+  
+  var twitterLink = document.getElementById("twitter-link");
+twitterLink.addEventListener("click", function() {
+  window.location.href = "https://twitter.com/arzuguneycaner";
+});
+
