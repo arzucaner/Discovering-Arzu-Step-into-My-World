f0140e74fe8e74095a6fc72f53f1f6eaacb42900
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-02 22:12:19 +0100

    Add Contact form

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ twitterLink.addEventListener("click", function() {
   window.location.href = "https://twitter.com/arzuguneycaner";
 });
 
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+   
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+  
+    document.getElementById('contact-form').reset();
+  });
+  
